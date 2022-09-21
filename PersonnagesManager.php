893fc633d4e91010b7371fef1ebf46afe7aab2ea
Kozen1 <benji6127@hotmail.fr>
2022-09-21 17:01:43 +0200

    Fourth commit

--- PersonnagesManager.php
+++ PersonnagesManager.php
@@ -8,10 +8,35 @@ class PersonnagesManager {
     }
 
     public function add(Personnage $perso) {
+        $request = $this->_db->prepare('INSERT INTO personnages SET nom = :nom,
+            `force` = :force,  pv = :pv, experience = :experience;');
 
+            $request->bindValue(':nom', $perso->getNom(), PDO::PARAM_STR);
+            $request->bindValue(':force', $perso->getForce(), PDO::PARAM_STR);
+            $request->bindValue(':pv', $perso->getPv(), PDO::PARAM_STR);
+            $request->bindValue(':experience', $perso->getExperience(), PDO::PARAM_STR);
+
+        $request->execute();
+
+        if ($request->errorCode() > 0) {
+            echo "<br/>Une erreur SQL est intervenue : ";
+            print_r($request->errorInfo()[2]);
+        }
     }
 
     public function delete(Personnage $perso) {
 
+    }
+    public function getOne($id){
+
+    }
+    public function getList(){
+        
+    }
+    public function update(Personnage $perso){
+        
+    }
+    public function setDb(PDO $db){
+        $this->_db = $db;
     }
 }
